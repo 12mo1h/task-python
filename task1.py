7ac93580f299c37326eb2b39218c66c5a0884251
12mo1h <mld3mk@gmail.com>
2025-11-24 16:11:56 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+#mohamed ashraf salah 
+#task 1 
+item1=100
+iterm2=200
+item3=300
+budget=900
+total_cost=item1+iterm2+item3
+difference=budget-total_cost
+print("the total cost is: \n",total_cost)
+print("the budget is: \n",budget)
+print("the difference is: \n",difference)  
